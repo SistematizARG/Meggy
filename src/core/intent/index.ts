335eeb1cb5186f76add5c53073add83df294e36b
SistematizARG <125629996+SistematizARG@users.noreply.github.com>
2026-06-30 11:54:45 -0300

    Create index.ts

---
+++ src/core/intent/index.ts
@@ -0,0 +1,6 @@
+export * from "./intent.types.js";
+export * from "./intent.schema.js";
+export * from "./intent.factory.js";
+export * from "./intent.validator.js";
+export * from "./intent.engine.js";
+export * from "./errors.js";
